294c5f08d018d4f82727a02a33c3d9bde426ca99
tanxw <tanxw@kaisagroup.com>
2020-04-15 16:31:23 +0800

    示例练习

---
+++ week05/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/15 10:21
+# @Author  : tanxw
---
+++ week05/mongo_example.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/15 10:22
+# @Author  : tanxw
+
+#pip install pymongo
+import pymongo
+import json
+
+# 1.链接数据库
+client = pymongo.MongoClient(host="127.0.0.1", port=27017)
+
+# 2. 建库
+db = client['scrapy']
+
+# 3. 建表
+collection = db['test']
+
+# collection = client['pystu']['python']
+# collection = client.pystu.python
+
+# 4.把json文件的数据 插入到数据库 insert()
+data_list = json.load(open('test.json', 'r'))
+collection.insert_many(data_list)
+
+# 5. 关闭
+client.close()
